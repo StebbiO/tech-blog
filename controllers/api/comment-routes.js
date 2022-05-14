021972cda4faf5febc910d5cc0f15e25ebf102be
Stebbi <stefodd@yahoo.com>
2022-05-13 19:24:46 -0500

    comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll().then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            text: req.body.text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        }).then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+});
+
+module.exports = router;
