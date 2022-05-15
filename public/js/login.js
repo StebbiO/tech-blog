56196afef7cab81ef38e7ec8e6ff40ce551cbe26
Stebbi <stefodd@yahoo.com>
2022-05-15 16:26:29 -0500

    login functionality works but does not display all of the dashboard page

--- public/js/login.js
+++ public/js/login.js
@@ -3,12 +3,12 @@
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('input[name="email-login"]').value.trim();
+    const password = document.querySelector('input[name="password-login"]').value.trim();
 
     if (email && password) {
         const response = await fetch('/api/users/login', {
-            method: 'post',
+            method: 'POST',
             body: JSON.stringify({
                 email,
                 password
@@ -28,13 +28,14 @@ async function loginFormHandler(event) {
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('input[name="username-signup"]').value.trim();
+    const email = document.querySelector('input[name="email-signup"]').value.trim();
+    const password = document.querySelector('input[name="password-signup"]').value.trim();
+    console.log(username, email, password);
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
-            method: 'post',
+            method: 'POST',
             body: JSON.stringify({
                 username,
                 email,
@@ -49,7 +50,7 @@ async function signupFormHandler(event) {
             alert(response.statusText);
         }
     }
-}
+};
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
